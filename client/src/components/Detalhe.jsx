c5fa55b7552ed3a33a209d0913f0d3c60d6108d5
Giovanni L <callmegiorgio@hotmail.com>
2022-12-16 13:36:00 -0300

    Agrupe as propriedades de um ponto turístico dentro de um só objeto no componente Detalhe

--- client/src/components/Detalhe.jsx
+++ client/src/components/Detalhe.jsx
@@ -9,22 +9,32 @@ export default function Detalhe(props) {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const [nome,       setNome]       = React.useState(location.state?.nome       || '');
-  const [estado,     setEstado]     = React.useState(location.state?.estado     || '');
-  const [cidade,     setCidade]     = React.useState(location.state?.cidade     || '');
-  const [referencia, setReferencia] = React.useState(location.state?.referencia || '');
-  const [descricao,  setDescricao]  = React.useState(location.state?.descricao  || '');
+  const [state, setState] = React.useState(location.state || {
+    nome: '',
+    estado: '',
+    cidade: '',
+    referencia: '',
+    descricao: ''
+  });
 
   function onEstadoChanged(estado) {
-    setEstado(estado);
+    setState(prevState => ({...prevState, estado: estado}));
   }
 
   function onCidadeChanged(cidade) {
-    setCidade(cidade);
+    setState(prevState => ({...prevState, cidade: cidade}));
+  }
+
+  // Função invocada quando os outros atributos além de estado e cidade
+  // são alterados, ou seja, 'nome', 'referencia' e 'descricao'.
+  function onOutrosChanged(event) {
+    const { name, value } = event.target;
+
+    setState(prevState => ({...prevState, [name]: value}));
   }
 
   function cadastrar() {
-    console.log('cadastrar')
+    console.log('cadastrar:', state)
   }
 
   return (
@@ -35,29 +45,32 @@ export default function Detalhe(props) {
         className='detalhe-right'
         type='text'
         readOnly={props.readOnly}
-        value={nome}
-        onChange={event => setNome(event.target.value)}
+        name='nome'
+        value={state.nome}
+        onChange={onOutrosChanged}
       />
       <p className='detalhe-localizacao bold'>Localização:</p>
       <p>UF/Cidade:</p>
       <div className='detalhe-estado'>
-        <EstadoDropDown readOnly={props.readOnly} onChange={onEstadoChanged} estado={estado}  />
-        <CidadeDropDown readOnly={props.readOnly} onChange={onCidadeChanged} estado={estado} cidade={cidade} />
+        <EstadoDropDown readOnly={props.readOnly} onChange={onEstadoChanged} estado={state.estado}  />
+        <CidadeDropDown readOnly={props.readOnly} onChange={onCidadeChanged} estado={state.estado} cidade={state.cidade} />
       </div>
       <p>Referência:</p>
       <input
         className='detalhe-right'
         type='text'
         readOnly={props.readOnly}
-        value={referencia}
-        onChange={event => setReferencia(event.target.value)}
+        name='referencia'
+        value={state.referencia}
+        onChange={onOutrosChanged}
       />
       <p className='bold'>Descrição:</p>
       <textarea
         className='detalhe-right'
         readOnly={props.readOnly}
-        value={descricao}
-        onChange={event => setDescricao(event.target.value)}
+        name='descricao'
+        value={state.descricao}
+        onChange={onOutrosChanged}
       />
       <button className='detalhe-voltar' onClick={() => navigate(-1)}>voltar</button>
       {
