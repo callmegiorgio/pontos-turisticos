b034171c018ede0e806ba0e3019f37d6d8a59a08
Giovanni L <callmegiorgio@hotmail.com>
2022-12-17 20:48:05 -0300

    Use o pacote 'cors' no servidor

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
+const cors       = require('cors');
 const db         = require('./database');
 
 const HTTP_PORT = 8000;
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
