4e39979cb9d5772c59c29e6df43839248d29ff4c
Giovanni L <callmegiorgio@hotmail.com>
2022-12-17 21:08:05 -0300

    Obtenha pontos turísticos do servidor

--- client/src/components/Pontos.jsx
+++ client/src/components/Pontos.jsx
@@ -5,45 +5,24 @@ import Inicio from './Inicio'
 import Ponto from './Ponto'
 import './Pontos.css'
 
-function makePontos(count = 5) {
-  const cristo_redentor = {
-    nome: "Cristo Redentor",
-    cidade: "Rio de Janeiro",
-    estado: "RJ",
-    referencia: "Morro do Corcovado",
-    descricao: 
-      "Cristo Redentor é uma estátua art déco que retrata Jesus Cristo, " +
-      "localizada no topo do morro do Corcovado, a 709 metros acima do nível do mar, " + 
-      "com vista para parte considerável da cidade brasileira do Rio de Janeiro. " +
-      "Feito de concreto armado e pedra-sabão, tem trinta metros de altura " + 
-      "(uma das maiores estátuas do mundo), sem contar os oito metros do pedestal. " +
-      "Seus braços se esticam por 28 metros de largura e a estrutura pesa 1145 toneladas."
-  };
-
-  const pontos = [];
-
-  while (count--)
-    pontos.push(cristo_redentor);
-
-  return pontos;
-}
-
-/// Fake an HTTPS request for now.
-function queryPontos(termo) {
-  if (termo === ':teste')
-    return makePontos();
-  else
-    return [];
-}
-
 export default function Pontos() {
   const [searchParams, setSearchParams] = useSearchParams()
+  const [pontos, setPontos] = React.useState([]);
 
   const termo = searchParams.get('busca')
-  const pontos = queryPontos(termo)
+
   const pontoElementos = pontos.map(
     ponto => <Ponto key={nanoid()} {...ponto} />
   )
+
+  React.useEffect(() => {
+    fetch(`http://localhost:8000/api/pontos/${termo}`)
+    .then(res => res.json())
+    .then(res => {
+      if (res.message === 'success')
+        setPontos(res.data);
+    });
+  }, [termo]);
   
   return (
     <div className='pontos'>
