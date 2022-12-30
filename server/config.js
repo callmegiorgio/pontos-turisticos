c7d6e500e4c7b5955de347f1f63f0153ff8ab987
Giovanni L <callmegiorgio@hotmail.com>
2022-12-30 12:05:45 -0300

    Permita que o banco de dados seja definido pelas variáveis de ambiente `DB_TYPE` e `DB_CONN`

--- server/config.js
+++ server/config.js
@@ -2,7 +2,37 @@ require('dotenv').config();
 
 const env = process.env;
 
+function readDatabaseConfig() {
+  let db_type = (env.DB_TYPE || '').toLowerCase();
+  let db_conn = env.DB_CONN || '';
+
+  switch (db_type) {
+    case 'sqlite':
+    case 'mssql':
+        break;
+    
+    default:
+      if (db_type !== '') {
+        console.warn(
+          `Environment variable 'DB_TYPE' has invalid value '${env.DB_TYPE}'. ` +
+          "Falling back to in-memory SQLite connection."
+        );
+      }
+
+      db_type = 'sqlite';
+      db_conn = ':memory:';
+  }
+
+  if (db_conn === '' && db_type == 'sqlite')
+    db_conn = ':memory:';
+
+  return {
+    type: db_type,
+    conn: db_conn
+  };
+}
+
 module.exports = {
-    db_file: env.DB_FILE || ':memory:',
+    db: readDatabaseConfig(),
     port: env.PORT || 8000
 }
--- server/services/database.js
+++ server/services/database.js
@@ -1,6 +1,18 @@
-const { db_file }    = require('../config');
+const config         = require('../config');
 const SQLiteDatabase = require('./sqlite-database');
+const MSSQLDatabase  = require('./mssql-database');
 
-const db = new SQLiteDatabase(db_file);
+function createDatabase() {
+  switch (config.db.type) {
+    case 'sqlite':
+      return new SQLiteDatabase(config.db.conn);
+    
+    case 'mssql':
+      return new MSSQLDatabase(config.db.conn);
+    
+    default:
+      throw new Error(`Unknown database type '${config.db.type}'`);
+  }
+}
 
-module.exports = db;
+module.exports = createDatabase();
